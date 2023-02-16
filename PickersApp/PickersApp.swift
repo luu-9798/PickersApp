7a7f6f84bf7a936b3f946d23868092f806bb023b
tluu9798 <trungluu9798@gmail.com>
2023-02-16 15:04:57 -0500

    Initial commit

---
+++ PickersApp/PickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickersAppApp.swift
+//  PickersApp
+//
+//  Created by Trung Luu on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct PickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
